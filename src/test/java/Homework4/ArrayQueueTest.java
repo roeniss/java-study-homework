2112471e44d05f2cec9f764e12efc1cbb769cdae
roeniss(문성혁) <roeniss.milk@kakaocorp.com>
2020-11-29 23:45:17 +0900

    refactoring

--- src/test/java/Homework4/ArrayQueueTest.java
+++ src/test/java/Homework4/ArrayQueueTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ArrayQueueTest {
--- src/test/java/Homework4/ListNodeStackTest.java
+++ src/test/java/Homework4/ListNodeStackTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ListNodeStackTest {
   ListNodeStack stack;
