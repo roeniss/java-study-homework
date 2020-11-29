d0c706066fcbef5925e2b52ff6d6a5d29316bbc1
roeniss(문성혁) <roeniss.milk@kakaocorp.com>
2020-11-29 23:24:47 +0900

    refactoring

--- src/main/java/Homework4/Main.java
+++ src/main/java/Homework4/Main.java
@@ -1,16 +1,5 @@
 package Homework4;
 
-import org.kohsuke.github.GHIssue;
-
-import java.util.List;
-import java.util.Map;
-
 public class Main {
-  public static void main(String[] args) {
-    String token = System.getenv("token");
-    GithubClient dashboard = new GithubClient(token);
-    List<GHIssue> issues = dashboard.getIssues("whiteship/live-study");
-    Map<String, List<GHIssue>> participatePerUser = dashboard.getComments(issues);
-    dashboard.showStatistics(participatePerUser);
-  }
+  public static void main(String[] args) {}
 }
